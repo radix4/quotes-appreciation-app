e852f7e4e2d1ddc8cbd94d6e9ec55945eb2bdece
Thang Cao <shiyoline@gmail.com>
2021-02-04 20:22:07 -0800

    Connect backend to the database

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,45 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
-let users = [
-  {
-    username: 'thang',
-    password: 'shokunin',
-  },
-  {
-    username: 'shiro',
-    password: 'crystal',
-  },
-  {
-    username: 'aristotle',
-    password: 'iscool',
-  },
-]
+const url = process.env.TEST_MONGODB_URI
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+// connect to database
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+// define a schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  date: Date,
+  important: Boolean,
 })
 
+//"User" model becomes "users" collection in mongodb
+const User = mongoose.model('User', userSchema)
+
+// render users from database to browser
 app.get('/api/users', (request, response) => {
-  response.json(users)
+  User.find({}).then((users) => {
+    response.json(users)
+  })
 })
 
-const PORT = 3001
+// express app.listen() allows port to open on browser
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
